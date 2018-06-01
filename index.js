657b6da7024914ef3877450fde08ca4785589417
Bradley Moore <bradleym007@gmail.com>
2018-06-01 11:32:31 -0700

    Changed site query var to domain

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var whois = require('whois');
 
 exports.whois = function whois(req, res){
   var url;
-  if(req.body.site){
-    url = parseUrl(req.body.site);
-  }else if(req.query && req.query.site){
-    url = parseUrl(req.query.site);
+  if(req.body.domain){
+    url = parseUrl(req.body.domain);
+  }else if(req.query && req.query.domain){
+    url = parseUrl(req.query.domain);
   }else{
-    return res.status( 420 ).send( 'Missing required \'site\' parameter.' );
+    return res.status( 420 ).send( 'Missing required \'domain\' parameter.' );
   }
   
   whois.lookup(url, function(err, data) {
