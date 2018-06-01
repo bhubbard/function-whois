c6b2a0a27cce45b28f8cabe7e0c8659ca70b8d91
Bradley Moore <bradleym007@gmail.com>
2018-06-01 11:12:44 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var whois = require('whois');
+
+exports.whois = function whois(req, res){
+  var url;
+  if(req.body.site){
+    url = parseUrl(req.body.site);
+  }else if(req.query && req.query.site){
+    url = parseUrl(req.query.site);
+  }else{
+    return res.status( 420 ).send( 'Missing required \'site\' parameter.' );
+  }
+  
+  whois.lookup(url, function(err, data) {
+    if(err){
+      return res.status( 500 ).send( 'Something went wrong on our end.' );
+    }else{
+      console.log(data);
+    }
+  });
+}
